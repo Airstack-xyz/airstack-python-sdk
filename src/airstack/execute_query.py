9acc16f60d1869eaba1b69d66e4b65c69b914b67
Sarvesh Singh <40350507+sarvesh371@users.noreply.github.com>
2023-06-19 13:14:10 +0530

    fixing python sdk for multi query (#3)
    * fixing python sdk for multi query
    
    * fixing python sdk for multi query

--- src/airstack/execute_query.py
+++ src/airstack/execute_query.py
@@ -4,17 +4,23 @@ Description: This module contains the methods to execute the query.
 """
 
 import json
+import warnings
 import re
-from graphql import parse, print_ast
+from graphql import parse, print_ast, visit
 from airstack.send_request import SendRequest
+from graphql.language.ast import OperationDefinition
 from airstack.generic import (
     find_page_info,
     add_cursor_to_input_field,
     replace_cursor_value,
-    has_cursor
+    has_cursor,
+    RemoveQueryByStartingName,
+    add_page_info_to_queries,
+    remove_unused_variables
 )
 from airstack.constant import AirstackConstants
 
+warnings.filterwarnings("ignore", message="coroutine .* was never awaited")
 
 class QueryResponse:
     """Class for generate the query response
@@ -32,7 +38,7 @@ class QueryResponse:
             get_next_page (func, optional): func to get the next page data. Defaults to None.
             get_prev_page (func, optional): func to get the previous page data. Defaults to None.
         """
-        self.response = response
+        self.data = response
         self.status_code = status_code
         self.error = error
         self.has_next_page = has_next_page
@@ -60,8 +66,27 @@ class AirstackClient:
 
         self.timeout = AirstackConstants.API_TIMEOUT
         self.api_key = api_key
+    
+    def create_execute_query_object(self, query=None, variables=None):
+        # Create a new object using the caller's `self` variables
+        execute_query = ExecuteQuery(query=query, variables=variables, url=self.url, api_key=self.api_key, timeout=self.timeout)
+        return execute_query
 
-    async def execute_query(self, query=None, variables=None):
+class ExecuteQuery:
+    """Class to execute query functions
+
+    Returns:
+        object: object of execute query
+    """
+    def __init__(self, query=None, variables=None, url=None, api_key=None, timeout=None):
+        self.deleted_queries = []
+        self.query = query
+        self.variables = variables
+        self.url = url
+        self.api_key = api_key
+        self.timeout = timeout
+
+    async def execute_query(self, query=None):
         """Async function to run a GraphQL query and get the data
 
         Args:
@@ -73,13 +98,16 @@ class AirstackClient:
             Tuple: GraphQL response data or None, GraphQL response status code,
             error message or None
         """
+        if query is None:
+            query = self.query
+            
         headers = {
             'Content-Type': 'application/json',
             'Authorization': self.api_key
         }
         payload = {
             'query': query,
-            'variables': variables
+            'variables': self.variables
         }
 
         response, status_code, error = await SendRequest.send_post_request(
@@ -99,65 +127,90 @@ class AirstackClient:
             error message or None, next cursor,
             previous cursor
         """
+        if query is None:
+            query = self.query
+
         regex = re.compile(r'pageInfo')
         has_page_info = regex.search(query)
         if has_page_info is None:
-            return QueryResponse(None, None, "Pass query having pageInfo", None, None, None, None)
+            query = add_page_info_to_queries(query)
 
-        query_response = await self.execute_query(query, variables)
+        query_response = await self.execute_query(query=query)
         if query_response.error is not None:
             return QueryResponse(None, query_response.status_code, query_response.error, None, None, None, None)
 
         page_info = {}
-        for _key, value in query_response.response.items():
-            page_info[_key] = find_page_info(query_response.response[_key])
+        for _key, value in query_response.data.items():
+            page_info[_key] = find_page_info(query_response.data[_key])
 
         has_next_page = any(page_info['nextCursor'] != '' for page_info in page_info.values())
         has_prev_page = any(page_info['prevCursor'] != '' for page_info in page_info.values())
 
-        async def get_next_page():
-            """Func to get the next page data
+        return QueryResponse(query_response.data, query_response.status_code,
+                            query_response.error, has_next_page, has_prev_page,
+                            self.get_next_page(query, variables, page_info),
+                            self.get_prev_page(query, variables, page_info))
+
+    async def get_next_page(self, query, variables, page_info):
+        """Async function to get the next page data.
+
+        Args:
+            query (str): GraphQL query string.
+            variables (dict): Variables for the query.
+            page_info (dict): Page info dictionary.
+            deleted_queries (list): List to store deleted queries.
+            next_cursors (list): List to store next cursors.
+
+        Returns:
+            Tuple: GraphQL response data or None, GraphQL response status code,
+            error message or None, next cursor,
+            previous cursor
+        """
+        next_query = query
+        stored = False
+        for _page_info_key, _page_info_value in page_info.items():
+            document_ast = parse(next_query)
+            if _page_info_value['nextCursor'] == "":
+                self.deleted_queries.append(next_query)
+                stored = True
+                visitor = RemoveQueryByStartingName(query_start=_page_info_key)
+                document_ast = visit(document_ast, visitor)
+                next_query = remove_unused_variables(document_ast=document_ast, query=print_ast(document_ast))
+            else:
+                if not stored:
+                    self.deleted_queries.append(None)
+                if has_cursor(document_ast, _page_info_key):
+                    replace_cursor_value(document_ast, _page_info_key, _page_info_value['nextCursor'])
+                else:
+                    add_cursor_to_input_field(document_ast, _page_info_key, _page_info_value['nextCursor'])
+                next_query = print_ast(document_ast)
+        return await self.execute_paginated_query(next_query, variables)
+
+    async def get_prev_page(self, query, variables, page_info):
+        """Async function to get the previous page data.
+
+        Args:
+            query (str): GraphQL query string.
+            variables (dict): Variables for the query.
+            page_info (dict): Page info dictionary.
 
             Returns:
                 Tuple: GraphQL response data or None, GraphQL response status code,
                 error message or None, next cursor,
                 previous cursor
             """
+        deleted_query = self.deleted_queries.pop()
+        if deleted_query:
+            next_query = deleted_query
+        else:
             next_query = query
-            for _page_info_key, _page_info_value in page_info.items():
-                if _page_info_value['nextCursor'] == "":
-                    continue
-                document_ast = parse(next_query)
-                if has_cursor(document_ast, _page_info_key):
-                    replace_cursor_value(document_ast, _page_info_key,
-                    _page_info_value['nextCursor'])
-                else:
-                    add_cursor_to_input_field(document_ast, _page_info_key,
-                    _page_info_value['nextCursor'])
-                next_query = print_ast(document_ast)
-            return await self.execute_paginated_query(next_query, variables)
-
-        async def get_prev_page():
-            """Func to get the previous page data
-
-            Returns:
-                Tuple: GraphQL response data or None, GraphQL response status code,
-                error message or None, next cursor,
-                previous cursor
-            """
-            next_query = query
-            for _page_info_key, _page_info_value in page_info.items():
-                if _page_info_value['prevCursor'] == "":
-                    continue
-                document_ast = parse(next_query)
-                if has_cursor(document_ast, _page_info_key):
-                    replace_cursor_value(document_ast, _page_info_key,
-                    _page_info_value['prevCursor'])
-                else:
-                    add_cursor_to_input_field(document_ast, _page_info_key,
-                    _page_info_value['prevCursor'])
-                next_query = print_ast(document_ast)
-            return await self.execute_paginated_query(next_query, variables)
-
-        return QueryResponse(query_response.response, query_response.status_code,
-        query_response.error,has_next_page, has_prev_page,get_next_page, get_prev_page)
+        for _page_info_key, _page_info_value in page_info.items():
+            document_ast = parse(next_query)
+            if has_cursor(document_ast, _page_info_key):
+                replace_cursor_value(document_ast, _page_info_key,
+                _page_info_value['prevCursor'])
+            else:
+                add_cursor_to_input_field(document_ast, _page_info_key,
+                _page_info_value['prevCursor'])
+            next_query = print_ast(document_ast)
+        return await self.execute_paginated_query(next_query, variables)
--- src/example.py
+++ src/example.py
@@ -10,10 +10,6 @@ async def main():
   TokenBalances(
     input: {filter: {tokenAddress: {_eq: $name1}}, blockchain: ethereum}
   ) {
-    pageInfo {
-      nextCursor
-      prevCursor
-    }
     TokenBalance {
       chainId
       blockchain
@@ -25,10 +21,6 @@ async def main():
   test2: TokenTransfers(
     input: {filter: {tokenAddress: {_eq: $name2}}, blockchain: ethereum}
   ) {
-    pageInfo {
-      nextCursor
-      prevCursor
-    }
     TokenTransfer {
       chainId
       blockchain
@@ -43,14 +35,27 @@ async def main():
         "name2": "0xf4eced2f682ce333f96f2d8966c613ded8fc95dd",
     }
 
-    query_response = await api_client.execute_paginated_query(
-        query=query1, variables=variables1)
-
+    execute_query_client = api_client.create_execute_query_object(query=query1, variables=variables1)
+    query_response = await execute_query_client.execute_paginated_query()
     if query_response.has_next_page:
-        next_page_response = await query_response.get_next_page()
+        next_page_response = await query_response.get_next_page
+    if next_page_response.has_next_page:
+        next_page_response = await next_page_response.get_next_page
+    if next_page_response.has_next_page:
+        next_page_response = await next_page_response.get_next_page
     if next_page_response.has_prev_page:
-        pev_page_response = await query_response.get_prev_page()
-
+        prev_page_response = await next_page_response.get_prev_page
+    if prev_page_response.has_prev_page:
+        prev_page_response = await prev_page_response.get_prev_page
+    if prev_page_response.has_prev_page:
+        prev_page_response = await prev_page_response.get_prev_page
+    if prev_page_response.has_next_page:
+        next_page_response = await prev_page_response.get_next_page
+    if next_page_response.has_prev_page:
+        prev_page_response = await next_page_response.get_prev_page
+    if next_page_response.has_next_page:
+        next_page_response = await next_page_response.get_next_page
+    
     query = """
      query MyQuery($name1: Address!) {
       test1: TokenBalances(
@@ -78,15 +83,16 @@ async def main():
     if query_response.error:
         print(f"Error: {query_response.error}")
     else:
-        print(f"Response: {query_response.response}")
-
-    query_response = await api_client.execute_query(query=query, variables=variables)
+        print(f"Response: {query_response.data}")
+    
+    execute_query_client = api_client.create_execute_query_object(query=query, variables=variables)
+    query_response = await execute_query_client.execute_query()
 
     # Process the next page response
     if query_response.error:
         print(f"Error: {query_response.error}")
     else:
-        print(f"Response: {query_response.response}")
+        print(f"Response: {query_response.data}")
 
 
 asyncio.run(main())
